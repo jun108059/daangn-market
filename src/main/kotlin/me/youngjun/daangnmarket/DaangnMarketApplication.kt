a86dca82493ba2232e3725794e054181be468615
YoungJun Park <youngjun108059@gmail.com>
2022-12-25 15:45:32 +0900

    🎉 당근마켓 클론 코딩 프로젝트

---
+++ src/main/kotlin/me/youngjun/daangnmarket/DaangnMarketApplication.kt
@@ -0,0 +1,11 @@
+package me.youngjun.daangnmarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DaangnMarketApplication
+
+fun main(args: Array<String>) {
+    runApplication<DaangnMarketApplication>(*args)
+}
---
+++ src/test/kotlin/me/youngjun/daangnmarket/DaangnMarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.youngjun.daangnmarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DaangnMarketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
