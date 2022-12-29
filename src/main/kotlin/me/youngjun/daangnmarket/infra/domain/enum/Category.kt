47ed0f45d5727a1334cc02c8098cc5644fc381c6
YoungJun Park <youngjun108059@gmail.com>
2022-12-30 04:50:44 +0900

    🗃️카테고리 enum 초안 [#1]

---
+++ src/main/kotlin/me/youngjun/daangnmarket/infra/domain/enum/Category.kt
@@ -0,0 +1,24 @@
+package me.youngjun.daangnmarket.infra.domain.enum
+
+enum class Category(
+    val categoryName: String
+) {
+    A("디지털기기"),
+    B("생활가전"),
+    C("가구/인테리어"),
+    D("유아동"),
+    E("생활/가공식품"),
+    F("유아도서"),
+    G("스포츠/레저"),
+    H("여성잡화"),
+    I("여성의류"),
+    J("남성패션/잡화"),
+    K("게임/취미"),
+    L("뷰티/미용"),
+    M("반려동물용품"),
+    N("도서/티켓/음반"),
+    O("식물"),
+    P("기타 중고물품"),
+    Q("중고차");
+
+}
