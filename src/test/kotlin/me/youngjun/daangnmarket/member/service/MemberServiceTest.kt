43b096f58333caa386282cac380f44357f6e4e5a
YoungJun Park <youngjun108059@gmail.com>
2023-01-02 02:42:37 +0900

    ✅ 이메일 중복검사 테스트 추가 [#2]
    Repository 메서드 mocking 방식 다시 고민할 필요 있음
    
    Related to: #2

--- src/test/kotlin/me/youngjun/daangnmarket/member/service/MemberServiceTest.kt
+++ src/test/kotlin/me/youngjun/daangnmarket/member/service/MemberServiceTest.kt
@@ -6,8 +6,10 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verify
 import me.youngjun.daangnmarket.infra.domain.Member
+import me.youngjun.daangnmarket.infra.exception.DuplicationMemberException
 import me.youngjun.daangnmarket.member.dto.MemberJoinForm
 import me.youngjun.daangnmarket.member.repository.MemberRepository
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -57,4 +59,40 @@ internal class MemberServiceTest {
         assert(result.id == 1.toLong())
     }
 
+    @DisplayName("이메일 중복검사 테스트")
+    @Test
+    fun duplicateEmailTest() {
+        // given
+        val memberJoinForm = MemberJoinForm(
+            email = "youngjun@test.com",
+            password = "1234",
+            name = "박영준",
+            phone = "010-1234-1234",
+            nickname = "개발하는만두",
+            area = "판교",
+        )
+        val savedMember = Member(
+            id = 1,
+            email = "youngjun@test.com",
+            password = "1234",
+            name = "박영준",
+            phone = "010-1234-1234",
+            nickname = "개발하는만두",
+            area = "판교",
+        )
+
+        every {
+            memberRepository.save(any())
+        } returns savedMember
+
+        // when
+        every {
+            memberRepository.existsByEmail(any())
+        } returns true
+
+        // then
+        assertThrows(DuplicationMemberException::class.java) {
+            target.join(memberJoinForm)
+        }
+    }
 }
