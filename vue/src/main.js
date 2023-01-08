f18e151757084ab3590a3e3690d59225337fbd3e
YoungJun Park <youngjun108059@gmail.com>
2023-01-08 21:13:45 +0900

    ➕ vue-toastification 의존성 추가
    https://github.com/Maronato/vue-toastification
    
    Related to: #8

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,5 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
 import vuetify from "./plugins/vuetify";
+import Toast, { POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
-createApp(App).use(vuetify).use(router).mount("#app");
+const options = {
+  position: POSITION.TOP_RIGHT,
+  timeout: 2000,
+};
+
+createApp(App).use(vuetify).use(router).use(Toast, options).mount("#app");
