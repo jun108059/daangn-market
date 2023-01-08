9d11d105bfdaa5f050e07890e1483f4230fae61e
YoungJun Park <youngjun108059@gmail.com>
2023-01-09 05:28:09 +0900

    ➕ material-design-icons 의존성 추가 [#10]
    - 아이콘 사용을 위한 의존성
    
    Related to: #10

--- vue/src/plugins/vuetify.js
+++ vue/src/plugins/vuetify.js
@@ -1,10 +1,17 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
-
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 // Vuetify
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
