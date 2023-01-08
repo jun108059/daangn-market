9ff9c176fc453418b4810b5d4e8bb6919fb1c067
YoungJun Park <youngjun108059@gmail.com>
2023-01-08 21:11:31 +0900

    💄 회원가입 완료 컴포넌트 개발
    이메일 인증 기능 추후에 구현 필요
    
    Related to: #8

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/member/LoginView.vue";
 import JoinView from "@/views/member/JoinView.vue";
+import JoinOkView from "@/views/member/JoinOkView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -21,6 +22,11 @@ const router = createRouter({
       path: "/join",
       name: "Join",
       component: JoinView,
+    },
+    {
+      path: "/join-ok",
+      name: "JoinOk",
+      component: JoinOkView,
     },
   ],
 });
---
+++ vue/src/views/member/JoinOkView.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container fluid>
+    <v-row class="mb-8">
+      <v-col cols="12"><h2>🥳회원가입 완료 🎉</h2></v-col>
+    </v-row>
+    <v-layout class="mb-16">
+      <v-col cols="12" class="mb-16">
+        <h3>
+          입력하신 {{ email }} 로 <br />
+          인증 메일을 전송했습니다.<br /><br />
+          메일을 인증하고 <br />
+          Karrot 마켓을 시작해보세요.
+        </h3>
+      </v-col>
+    </v-layout>
+    <v-row>
+      <v-col cols="12" class="mt-16" style="color: red">구현 예정</v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" class="mt-16">
+        <v-btn href="/login" class="text-white" color="#ff931e" block>
+          로그인 하러가기
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        email: "이메일",
+      };
+    },
+  };
+</script>
+
+<style scoped>
+  .v-btn {
+    font-family: "EliceDigitalBaeum-Bd", serif;
+    font-size: 18px;
+    width: 300px;
+    height: 40px;
+  }
+</style>
