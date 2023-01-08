1194653bde86b0ceeb155a8ae7faa97c3b186718
YoungJun Park <youngjun108059@gmail.com>
2023-01-08 21:18:12 +0900

    🔧 Jpa 설정 파일 추가
    EnableJpaAuditing 애너테이션 활성화
    
    Related to: #1

---
+++ src/main/kotlin/me/youngjun/daangnmarket/infra/config/JpaConfig.kt
@@ -0,0 +1,9 @@
+package me.youngjun.daangnmarket.infra.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaConfig {
+}
