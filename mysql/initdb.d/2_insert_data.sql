aa9d63ea81cb143aa90c42488368899e61469e80
YoungJun Park <youngjun108059@gmail.com>
2023-01-14 21:13:54 +0900

    🧱️MySQL initdb, conf 추가
    - mysql 설정
    
    Related to: #19

---
+++ mysql/initdb.d/2_insert_data.sql
@@ -0,0 +1,4 @@
+insert
+into member (email, password, name, nickname, phone, area, role, created_at, updated_at)
+values ('youngjun108059@gmail.com', '$2a$10$4LbNcwtER6WCKM9lBFffwu62T4ETFaELybBQ3Nu8YG/ar.J35vNb.', '박영준', '만두',
+        '010-1234-1234', '판교', 'ROLE_USER', '2023-01-14 15:22:54.173854', '2023-01-14 15:22:54.173854')
