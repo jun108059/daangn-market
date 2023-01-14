dd51b601b48c522576a9bd746b0a1fffaf5d9071
YoungJun Park <youngjun108059@gmail.com>
2023-01-14 16:51:02 +0900

    👷️ CI 에러 수정(Mapstruct 수정)
    - BaseEntity 필드 ignore

--- src/main/kotlin/me/youngjun/daangnmarket/common/domain/mapping/MemberConverter.kt
+++ src/main/kotlin/me/youngjun/daangnmarket/common/domain/mapping/MemberConverter.kt
@@ -1,10 +1,13 @@
 package me.youngjun.daangnmarket.common.domain.mapping
 
-import me.youngjun.daangnmarket.common.domain.Member
 import me.youngjun.daangnmarket.api.member.dto.MemberJoinRequestDto
+import me.youngjun.daangnmarket.common.domain.Member
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 
 @Mapper
 interface MemberConverter {
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     fun convertToEntity(memberJoinRequestDto: MemberJoinRequestDto): Member
 }
