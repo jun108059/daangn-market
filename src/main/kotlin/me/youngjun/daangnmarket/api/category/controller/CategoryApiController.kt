bac5f1668d4e5a26204d9750ed7a5375682558af
YoungJun Park <youngjun108059@gmail.com>
2023-01-24 01:13:01 +0900

    👔 카테고리 조회 로직 추가 및 응답 데이터 변경 [#13]
    - ResponseEntity 사용
    - getCategory 함수 추가
    
    Related to: #13

--- src/main/kotlin/me/youngjun/daangnmarket/api/category/controller/CategoryApiController.kt
+++ src/main/kotlin/me/youngjun/daangnmarket/api/category/controller/CategoryApiController.kt
@@ -1,9 +1,9 @@
 package me.youngjun.daangnmarket.api.category.controller
 
-import me.youngjun.daangnmarket.api.category.dto.CategoryView
 import me.youngjun.daangnmarket.api.category.service.CategoryService
-import me.youngjun.daangnmarket.common.domain.dto.ApiResponse
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CategoryApiController(
@@ -11,8 +11,8 @@ class CategoryApiController(
 ) {
     @GetMapping("/api/v1/category/list")
     fun getCategoryListView(
-    ): ApiResponse<List<CategoryView>> {
+    ): ResponseEntity<Any> {
         val categoryList = categoryService.getCategoryList()
-        return ApiResponse.success(categoryList)
+        return ResponseEntity.ok(categoryList)
     }
 }
