82d22b4d757ceac3880fc9d77ed9d02833e9a203
YoungJun Park <youngjun108059@gmail.com>
2023-02-06 05:42:11 +0900

    ➕ Kotest 테스트 관련 의존성 추가
    - kotest.extensions : spring boot test 사용
    - ninja-squad : junit의 @MockBean @SpyBean 같은 기능 제공
    - mockk : unit test - mocking 사용
    
    Related to: #27

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,9 @@ dependencies {
     testImplementation("io.mockk:mockk:1.13.3")
     testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
     testImplementation("io.kotest:kotest-assertions-core:5.5.4")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
+    testImplementation("io.mockk:mockk:1.12.8")
 }
 
 tasks.withType<KotlinCompile> {
