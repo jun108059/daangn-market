72c2193efe2b2f9d2f7bed074672f4211deba128
YoungJun Park <youngjun108059@gmail.com>
2023-02-06 05:39:35 +0900

    ♻️ Product Status 패키지 변경 [#27]
    - QueryDSL 설정 검증
    
    Related to: #27

--- src/main/kotlin/me/youngjun/daangnmarket/api/product/dto/ProductFilterDto.kt
+++ src/main/kotlin/me/youngjun/daangnmarket/api/product/dto/ProductFilterDto.kt
@@ -1,9 +1,9 @@
 package me.youngjun.daangnmarket.api.product.dto
 
-import me.youngjun.daangnmarket.common.domain.enum.ProductStatus
+import me.youngjun.daangnmarket.common.domain.ProductStatus
 
 data class ProductFilterDto(
     val categoryId: Long?,
     val status: ProductStatus?,
-    val likes: Boolean,
+    val likes: Boolean?
 )
