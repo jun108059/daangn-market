d7dcb7e163a835e50bf03955dbd1c448e780d83a
YoungJun Park <youngjun108059@gmail.com>
2023-02-20 00:00:11 +0900

    🔧 store, axios 설정 (#18)
    Related to: #18

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,20 +1,24 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import App from "./App.vue";
+import store from "./store";
 import router from "@/router";
 import vuetify from "./plugins/vuetify";
-import Toast, { POSITION } from "vue-toastification";
+import Toast, {POSITION} from "vue-toastification";
 import "vue-toastification/dist/index.css";
-import { UpdateMedia, UploadMedia } from "vue-media-upload";
+import {UpdateMedia, UploadMedia} from "vue-media-upload";
+import axios from "axios";
 
 const options = {
-  position: POSITION.TOP_RIGHT,
-  timeout: 2000,
+    position: POSITION.TOP_RIGHT,
+    timeout: 2000,
 };
 
 createApp(App)
-  .use(vuetify)
-  .use(router)
-  .use(Toast, options)
-  .component("upload-media", UploadMedia)
-  .component("update-media", UpdateMedia)
-  .mount("#app");
+    .use(vuetify)
+    .use(router)
+    .use(Toast, options)
+    .use(store)
+    .provide('$axios', axios)
+    .component("upload-media", UploadMedia)
+    .component("update-media", UpdateMedia)
+    .mount("#app");
