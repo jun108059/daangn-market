98027217c9e6ce7bda3a5dab0bee26825e146441
YoungJun Park <youngjun108059@gmail.com>
2023-02-26 05:42:45 +0900

    👔 상품 상세 정보 필드 수정 (#12)
    - 상품 거래 상태
    - member id, 판매자 이미지 정보
    
    Related to: #12

--- src/main/kotlin/me/youngjun/daangnmarket/api/product/dto/ProductDetailView.kt
+++ src/main/kotlin/me/youngjun/daangnmarket/api/product/dto/ProductDetailView.kt
@@ -1,18 +1,25 @@
 package me.youngjun.daangnmarket.api.product.dto
 
 import com.fasterxml.jackson.annotation.JsonFormat
+import me.youngjun.daangnmarket.common.domain.ProductStatus
 import java.time.LocalDateTime
 
 data class ProductDetailView(
-    val imagePaths: List<String>,
-    val imagePathCount: Int,
-    val nickname: String,
+    val id: Long,
+    val memberId: Long,
+    val areaName: String,
     val title: String,
     val price: Long,
-    val category: String,
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    val createAt: LocalDateTime,
-    val content: String,
+    val likesCount: Int,
     val chatCount: Int,
-    val likesCount: Int
+    val status: ProductStatus,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    val createdDateTime: LocalDateTime,
+    val imageUrls: List<String>,
+    val nickname: String,
+    val category: String,
+    val content: String,
+    val sellerImageUrl: String?,
+    var isLikes: Boolean?,
+    var isMine: Boolean?,
 )
--- src/main/kotlin/me/youngjun/daangnmarket/common/domain/ProductStatus.kt
+++ src/main/kotlin/me/youngjun/daangnmarket/common/domain/ProductStatus.kt
@@ -1,9 +1,9 @@
 package me.youngjun.daangnmarket.common.domain
 
 enum class ProductStatus(
-    val value: String
+    val value: String,
 ) {
     TRADING("판매중"),
     RESERVED("예약중"),
-    COMPLETED("거래완료");
+    COMPLETED("거래완료"),
 }
