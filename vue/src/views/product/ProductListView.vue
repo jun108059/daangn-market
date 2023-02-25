a1cdec1ab6de6dae1245174be3f271f0275c768a
YoungJun Park <youngjun108059@gmail.com>
2023-02-25 23:42:27 +0900

    👔 판매상태, 등록일시 추가, 상세 조회 연결 (#43)
    - 거래 상태 추가, add-button 수정
    
    Related to: #43

--- vue/src/views/product/ProductListView.vue
+++ vue/src/views/product/ProductListView.vue
@@ -11,19 +11,26 @@
     <v-row
         class="mt-2 mb-2 product-row"
         v-for="(item, index) in state.list"
-        :key="index">
+        :key="index"
+        @click="goDetailPage(item.id)">
       <div style="float: left; width: 33%">
-        <img :src="item.imgUrl" alt="이미지" width="125"/>
+        <img :src="item.imgUrl" alt="이미지" width="125" height="100"/>
       </div>
       <div style="text-align: left; margin-left: 10px; width: 60%">
         <div class="title">{{ item?.title }}</div>
-        <div class="location">{{ item?.areaName }}</div>
-        <div class="price">{{ item?.price }}</div>
+        <div class="location">{{ item?.areaName }} • {{ $time.elapsedTime(item?.createdDateTime) }}</div>
+        <div class="price">
+          <div v-if="item?.status === 'RESERVED'" style="display:inline;">
+            <img class="state-img" :src="state.reservedImg" alt="예약중" width="55"/></div>
+          <div v-if="item?.status === 'COMPLETED'" style="display:inline;">
+            <img class="state-img" :src="state.completedImg" alt="거래완료" width="55"/></div>
+          {{ $money.currency(item?.price) }} 원
+        </div>
       </div>
-      <div style="position: absolute; bottom: 10px; right: 30px; width: 30%">
-        <img :src="state.chatImg" alt="채팅" width="20"/>
+      <div style="position: absolute; bottom: 10px; right: 5px; width: 30%; font-size: 14px">
+        <img class="chat-img" :src="state.chatImg" alt="채팅" width="15"/>
         {{ item?.chatCount }} &nbsp;
-        <img :src="state.heartImg" alt="좋아요" width="20"/>&nbsp;
+        <img class="like-img" :src="state.heartImg" alt="좋아요" width="15"/>&nbsp;
         {{ item?.likeCount }}
       </div>
     </v-row>
@@ -33,15 +40,14 @@
   </v-container>
   <v-row class="my-footer">
     <div style="float: left; width: 48%" class="homeButton">
-      <!--TODO("Home 선택된 image 추가")-->
       <router-link to="#">
-        <v-img class="mx-auto" width="80" src="@/assets/img/home.png"></v-img>
+        <v-img class="mx-auto" width="40" src="@/assets/img/home.png"></v-img>
       </router-link>
     </div>
     <div style="width: 48%">
       <!--TODO("나의 당근 페이지 연결")-->
       <router-link to="#">
-        <v-img class="mx-auto" width="75" src="@/assets/img/user-blank.png"></v-img>
+        <v-img class="mx-auto" width="40" src="@/assets/img/user-blank.png"></v-img>
       </router-link>
     </div>
   </v-row>
@@ -51,9 +57,12 @@
 import {onMounted, reactive} from "vue";
 import $axiosInst from "@/common/AxiosInst"
 import $store from "@/store/store"
+import router from "@/router";
 import chatImg from "@/assets/img/chat.png";
 import heartImg from "@/assets/img/heart.png";
-import addButton from "@/assets/img/addButton.png";
+import addButton from "@/assets/img/add-button.png";
+import reservedImg from "@/assets/img/reserved.png";
+import completedImg from "@/assets/img/completed.png";
 
 export default {
   setup() {
@@ -66,13 +75,16 @@ export default {
           price: "",
           likeCount: "",
           chatCount: "",
-          productStatus: "",
           imgUrl: "",
+          createdDateTime: "",
+          status: "",
         },
       ],
       chatImg: chatImg,
       heartImg: heartImg,
       addButton: addButton,
+      reservedImg: reservedImg,
+      completedImg: completedImg,
       userName: ""
     });
     onMounted(() => {
@@ -95,9 +107,16 @@ export default {
     };
     const setUserName = () => {
       state.userName = $store.getters.getUserName
-    }
+    };
+    const goDetailPage = (id) => {
+      router.push({
+        name: "ProductDetailView",
+        params: {productId: id}
+      })
+    };
     return {
       state,
+      goDetailPage,
     };
   }
 };
@@ -113,22 +132,35 @@ export default {
 
 .addButton {
   position: absolute;
-  bottom: 150px;
-  right: 20px;
+  bottom: 100px;
+  right: 0;
   width: 30%;
-  height: 80px;
-}
-
-.my-footer {
-  position: absolute;
-  bottom: 40px;
-  right: 20px;
-  width: 100%;
-  padding: 20px 30px 0 30px;
-  border-top: solid 3px #2c3e50;
+  height: 60px;
 }
 
 .homeButton {
-  border-right: solid 3px #2c3e50;
+  /*border-right: solid 3px #2c3e50;*/
+  margin-left: 1px;
+}
+
+.location {
+  font-size: 14px;
+  color: gray;
+}
+
+.state-img {
+  vertical-align: middle;
+  margin-top: -2px;
+  margin-left: -10px;
+  margin-right: -5px;
+}
+
+.chat-img {
+  vertical-align: middle;
+}
+
+.like-img {
+  vertical-align: middle;
+  margin-right: -3px;
 }
 </style>
