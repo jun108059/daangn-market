d03985caab3b53ae9ed70f7d939b50e576a85636
YoungJun Park <youngjun108059@gmail.com>
2023-02-26 05:41:23 +0900

    ✨ 상품 상세 페이지 (#12)
    - 슬라이더 이미지 뷰어
    - 판매자 정보
    - 상품 상세 정보
    
    Related to: #12

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/member/LoginView.vue";
@@ -6,41 +6,47 @@ import JoinView from "@/views/member/JoinView.vue";
 import JoinOkView from "@/views/member/JoinOkView.vue";
 import ProductListView from "@/views/product/ProductListView.vue";
 import ProductRegisterView from "@/views/product/ProductRegisterView.vue";
+import ProductDetailView from "@/views/product/ProductDetailView.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "Home",
-      component: HomeView,
-    },
-    {
-      path: "/login",
-      name: "Login",
-      component: LoginView,
-    },
-    {
-      path: "/join",
-      name: "Join",
-      component: JoinView,
-    },
-    {
-      path: "/join-ok",
-      name: "JoinOk",
-      component: JoinOkView,
-    },
-    {
-      path: "/product/list",
-      name: "ProductList",
-      component: ProductListView,
-    },
-    {
-      path: "/product/register",
-      name: "ProductRegister",
-      component: ProductRegisterView,
-    },
-  ],
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "/",
+            name: "Home",
+            component: HomeView,
+        },
+        {
+            path: "/login",
+            name: "Login",
+            component: LoginView,
+        },
+        {
+            path: "/join",
+            name: "Join",
+            component: JoinView,
+        },
+        {
+            path: "/join-ok",
+            name: "JoinOk",
+            component: JoinOkView,
+        },
+        {
+            path: "/product/list",
+            name: "ProductList",
+            component: ProductListView,
+        },
+        {
+            path: "/product/detail/:productId",
+            name: "ProductDetailView",
+            component: ProductDetailView,
+        },
+        {
+            path: "/product/register",
+            name: "ProductRegister",
+            component: ProductRegisterView,
+        },
+    ],
 });
 
 export default router;
