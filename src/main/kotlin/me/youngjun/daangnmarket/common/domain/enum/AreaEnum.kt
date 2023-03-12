2ef744aa55c964e53859f52d1207e4f1aa54725b
YoungJun Park <youngjun108059@gmail.com>
2023-03-12 18:51:13 +0900

    ♻️ AreaEnum 데이터 수정 (#45)
    - 데이터베이스 값에 맞추도록 변경
    
    Related to: #45

--- src/main/kotlin/me/youngjun/daangnmarket/common/domain/enum/AreaEnum.kt
+++ src/main/kotlin/me/youngjun/daangnmarket/common/domain/enum/AreaEnum.kt
@@ -2,21 +2,22 @@ package me.youngjun.daangnmarket.common.domain.enum
 
 enum class AreaEnum(
     val areaCode: String,
-    val areaName: String
+    val areaName: String,
 ) {
-    PANGYO("0001", "판교"),
-    SUJI("0002", "수지"),
-    JAMSIL("0003", "잠실");
+    PANGYO("1", "판교"),
+    SUJI("2", "수지"),
+    JAMSIL("3", "잠실"),
+    ;
 
     companion object {
         fun of(
-            areaCode: String
+            areaCode: String,
         ): AreaEnum? = values().firstOrNull() {
             it.areaCode == areaCode
         }
 
         fun getName(
-            areaCode: String?
+            areaCode: String?,
         ): String {
             AreaEnum.values().iterator().forEach {
                 if (it.areaCode == areaCode) {
